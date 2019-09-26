d6f97d9d67e095eb754d188b036a062e02543145
José Frias <josefrias99@ua.pt>
2019-09-26 18:19:07 +0100

    C_Documentation

---
+++ cProject/html/search/all_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['special1distances_2etxt_8',['Special1distances.txt',['../Special1distances_8txt.html',1,'']]]
+];
---
+++ cProject/html/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['name_22',['name',['../cities_8h.html#abc1e86d7c344fe34ff09e72d4595ab7e',1,'cities.h']]]
+];
